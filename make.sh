930b92a8d5a3c6db9ad62a423fa2a747cf343b26
Michael D. Hoyle <hoylemd@gmail.com>
2015-11-15 16:53:58 -0500

    add nginx conf and deployment

--- make.sh
+++ make.sh
@@ -2,15 +2,24 @@
 
 command=$1
 
-usage="usage: ./make.sh [install|server]\n"
+usage="usage: ./make.sh [install|server|deploy]\n"
 usage="$usage install - Install node dependancies\n"
-usage="$usage firsttime = Execute first-time setup\n"
+usage="$usage server - run dev server\n"
+usage="$usage deploy $hostname - deploy to a host\n"
 
 if [ "$command" = "install" ]; then
   npm install
   bower install
 elif [ "$command" = "server" ]; then
   ember server
+elif [ "$command" = "deploy" ]; then
+  hostname="$2"
+  site_path="$hostname:/var/www/peaches-website"
+  nginx_path="$hostname:/etc/nginx/sites-available/peaches-website.conf"
+  ember build --environment=production
+  echo "copying distributable files to"
+  scp -r dist/ $site_path
+  scp nginx.conf $nginx_path
 else
   echo "$usage"
 fi
--- makefile
+++ makefile
@@ -1,5 +1,10 @@
+PROD_HOST=dorf
+
 install : make.sh
 	./make.sh install
 
 server : make.sh
 	./make.sh server
+
+deploy: make.sh
+	./make.sh deploy $(PROD_HOST)
