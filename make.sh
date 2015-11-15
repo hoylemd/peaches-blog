b3e0ad1759b361000a782e14340b3fd66942686a
Michael D. Hoyle <hoylemd@gmail.com>
2015-11-15 14:18:09 -0500

    finish packaging

--- make.sh
+++ make.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 command=$1
+nenv_path="nenv"
 
-usage="usage: ./make.sh [install|freeze|firsttime]\n"
+usage="usage: ./make.sh [install|firsttime]\n"
 usage="$usage install - Install node dependancies\n"
-usage="$usage freeze = Update dependancies\n"
 usage="$usage firsttime = Execute first-time setup\n"
 
 if [ "$command" = "install" ]; then
   echo "Installing!"
-elif [ "$command" = "freeze" ]; then
-  echo "Freezing!"
+  source "$nenv_path/bin/activate"
+  npm install
 elif [ "$command" = "firsttime" ]; then
-  echo "First-time setup!"
+  if [ -d "$nenv_path" ]; then
+    echo "First-time setup already done. Nothing to do!"
+  else
+	  echo "First-time setup!"
+	  nodeenv --prebuilt "$nenv_path"
+	  $result = $?
+	  if $result; then
+	    echo "environment creation failed. Is nodeenv installed?"
+	  else
+	    ./make.sh install
+	  fi
+  fi
 else
   echo "$usage"
 fi
---
+++ makefile
@@ -0,0 +1,8 @@
+install : make.sh
+	./make.sh install
+
+freeze : make.sh
+	./make.sh freeze
+
+firsttime : make.sh
+	./make.sh firsttime
