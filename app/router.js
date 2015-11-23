560a65af08eacfebf8931d03b2ef35d250883089
Michael D. Hoyle <hoylemd@gmail.com>
2015-11-23 17:15:29 -0500

    add home template

--- app/router.js
+++ app/router.js
@@ -6,6 +6,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home', { path: '/' });
+  this.route('blog', function () {
+    this.route('posts');
+    this.route('post', { path: '/post/:post_id' });
+  });
+  this.route('stuff', function() {
+    this.route('falloutjunk');
+  });
+  this.route('page-not-found', { path: '/*wildcard' });
 });
 
 export default Router;
