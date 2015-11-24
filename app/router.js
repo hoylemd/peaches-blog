89fa49ce24a5b62b4886846af4554d1436e66934
Michael D. Hoyle <hoylemd@gmail.com>
2015-11-23 20:37:03 -0500

    make post template

--- app/router.js
+++ app/router.js
@@ -7,8 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('home', { path: '/' });
-  this.route('blog', function () {
-    this.route('posts');
+  this.route('blog', function() {
     this.route('post', { path: '/post/:post_id' });
   });
   this.route('stuff', function() {
